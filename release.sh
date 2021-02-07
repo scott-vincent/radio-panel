740c6aaf073aa4b08292759ca0766cefc6b70e67
Scott Vincent <scottvincent@yahoo.com>
2021-02-07 07:24:18 +0000

    Prepare first release

---
+++ release.sh
@@ -0,0 +1,10 @@
+rel=v1.0.0
+mkdir release >/dev/null 2>&1
+rm -rf release/$rel >/dev/null 2>&1
+mkdir release/$rel
+cp radio-panel/radio-panel release/$rel
+cp -rp radio-panel/settings release/$rel
+sudo chown pi:pi release/$rel/settings/*.json
+dos2unix release/$rel/settings/*.json
+cp release/$rel/settings/default-settings.json release/$rel/settings/radio-panel.json
+tar -zcvf release/radio-panel-$rel-raspi4.tar.gz release/$rel
